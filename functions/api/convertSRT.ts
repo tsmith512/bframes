bac309977d66a2a6849c7e3e97b356779c6272e7
Taylor Smith <tsmith@cloudflare.com>
2024-04-18 14:32:58 -0500

    Add a Pages Function handler for this

---
+++ functions/api/convertSRT.ts
@@ -0,0 +1,80 @@
+// Be sure to also set nodejs_compat flag in wrangler.toml or Pages Functions config
+// import { Buffer } from 'node:buffer';
+
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    passThroughOnException, // same as ctx.passThroughOnException in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+
+  if (request.method === 'GET') {
+    return new Response('Please POST SRT content in the request body.', { status: 405 });
+  }
+
+  const input = await request.text();
+
+  const output = srtToVtt(input);
+
+  return new Response(output, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/vtt',
+      'Cache-Control': 'max-age=0, no-cache, must-revalidate',
+    }
+  });
+}
+
+const convertCue = (input: string): string => {
+  // EXAMPLES:
+
+  // 1
+  // 00:00:03,395 --> 00:00:06,728
+  // Captain's Log, Stardate 44286.5.
+
+  // 2
+  // 00:00:06,765 --> 00:00:09,165
+  // The <i>Enterprise</i> is conducting
+  // a security survey
+
+  const [number, time, ...content] = input.split('\n');
+
+  // @TODO: Validate cue number?
+  const newNumber = number;
+
+  // @TODO: Validate timestamp?
+  const newTime = time
+    // Milliseconds should be noted with a period, not comma
+    .replace(/(\d\d:\d\d:\d\d),(\d\d\d)/g, '$1.$2')
+    // @TODO: Remove coordinates for now
+    .replace(/[XY][12]:.*/g, '')
+  ;
+
+  const newContent = content
+    // @TODO: Eliminate newlines (spec suggestion but sometimes breaks editorially desired?)
+    .join(' ')
+    // Remove HTML tags that aren't <i> <b> or <u>
+    .replace(/<\/?[^ibu/][^>]*>/gi, '')
+    // Convert {ibu} syntax to HTML tag
+    .replace(/\{([ibu])\}/g, '<$1>')
+    .replace(/\{\/([ibu])\}/g, '</$1>')
+  ;
+
+  return [newNumber, newTime, newContent].join('\n');
+};
+
+const srtToVtt = (input: string): string => {
+  // Get input, trim leading/trailing whitepace, and remove carriage returns
+  const srt = input.trim().replace(/\r+/g, '');
+
+  // Split the cue stack
+  const srtCues = srt.split('\n\n');
+
+  const vttCues = srtCues.map(cue => convertCue(cue))
+  return ['WEBVTT FILE', ...vttCues].join('\n\n');
+};
