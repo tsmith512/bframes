30662607838efd90b355273d07fa67ae744ffedd
Taylor Smith <tsmith@cloudflare.com>
2025-05-23 14:33:17 -0500

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git fetch --unshallow
+
 mkdir -p static/vendor/mermaid
 curl https://registry.npmjs.org/mermaid/-/mermaid-10.6.1.tgz -o mermaid.tgz
 tar -zxf mermaid.tgz -C static/vendor/mermaid
